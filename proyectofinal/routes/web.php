ef8ae71f279ad8a350ce8651a9c10a46251e87f4
jgarciavega <jgarcia_20@alu.uabcs.mx>
2023-11-18 19:00:38 -0700

    Añadir rutas

--- proyectofinal/routes/web.php
+++ proyectofinal/routes/web.php
@@ -11,6 +11,31 @@ Route::get('/index', function () {
     return view('index');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/productos', function () {
+    return view('productos');
 });
+
+Route::get('/usuarios', function () {
+    return view('usuarios');
+});
+
+Route::get('/cupones', function () {
+    return view('cupones');
+});
+
+Route::get('/ordenes', function () {
+    return view('ordenes');
+});
+
+Route::get('/inventarios', function () {
+    return view('inventarios');
+});
+
+Route::get('/catalogos', function () {
+    return view('catalogos');
+});
+
+Route::get('/configuraciones', function () {
+    return view('configuraciones');
+});
+
