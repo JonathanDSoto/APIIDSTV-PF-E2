f5da9f78bc5d22bb102b4f065f15748162ea7619
jgarciavega <jgarcia_20@alu.uabcs.mx>
2023-11-18 19:02:28 -0700

    se añadieron rutas

---
+++ proyectofinal/resources/views/index.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed layout-compact " dir="ltr" data-theme="theme-default" data-assets-path="../../assets/" data-template="vertical-menu-template">
+@include("layouts.header")
+<body>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DDHKGP" height="0" width="0" style="display: none; visibility: hidden"></iframe></noscript>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar  ">
+        <div class="layout-container">
+            <!-- Menu -->
+
+            @include("layouts.sidebar")
+
+            <!-- / Menu -->
+
+            <!-- Layout container -->
+            <div class="layout-page">
+
+                <!-- Navbar -->
+                @include("layouts.navbar")
+                <!-- / Navbar -->
+
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+                    <!-- Content -->
+                    <div class="container-xxl flex-grow-1 container-p-y">
+
+                        <div class="row">
+                           <h1>Informacion de index</h1>
+                        </div>
+
+                    </div>
+                    <!-- / Content -->
+
+                    <div class="content-backdrop fade"></div>
+                </div>
+                <!-- Content wrapper -->
+            </div>
+            <!-- / Layout page -->
+        </div>
+
+    </div>
+    <!-- / Layout wrapper -->
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    @include("layouts.scripts")
+
+</body>
+
+</html>
--- proyectofinal/resources/views/login.blade.php
+++ proyectofinal/resources/views/login.blade.php
@@ -5,10 +5,9 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-
-  <title>Login Basic - Pages | Vuexy - Bootstrap Admin Template</title>
-
-
+ 
+  <title>WebStore </title>
+  
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 5" />
   <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5">
   <!-- Canonical SEO -->
@@ -35,7 +34,7 @@
   <!-- End Google Tag Manager -->
 
   <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="{{asset('/assets/img/favicon/favicon.ico')}}" />
 
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -43,37 +42,38 @@
   <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;ampdisplay=swap" rel="stylesheet">
 
   <!-- Icons -->
-  <link rel="stylesheet" href="../../assets/vendor/fonts/fontawesome.css" />
-  <link rel="stylesheet" href="../../assets/vendor/fonts/tabler-icons.css" />
-  <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
+  <link rel="stylesheet" href="{{asset('/assets/vendor/fonts/fontawesome.css')}}" />
+  <link rel="stylesheet" href="{{asset('/assets/vendor/fonts/tabler-icons.css')}}" />
+  <link rel="stylesheet" href="{{asset('/assets/vendor/fonts/flag-icons.css')}}" />
 
   <!-- Core CSS -->
-  <link rel="stylesheet" href="../../assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
-  <link rel="stylesheet" href="../../assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
-  <link rel="stylesheet" href="../../assets/css/demo.css" />
+  <link rel="stylesheet" href="{{asset('/assets/vendor/css/rtl/core.css')}}" class="template-customizer-core-css" />
+  <link rel="stylesheet" href="{{asset('/assets/vendor/css/rtl/theme-default.css')}}" class="template-customizer-theme-css" />
+  <link rel="stylesheet" href="{{asset('/assets/css/demo.css')}}" />
 
   <!-- Vendors CSS -->
-  <link rel="stylesheet" href="../../assets/vendor/libs/node-waves/node-waves.css" />
-  <link rel="stylesheet" href="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-  <link rel="stylesheet" href="../../assets/vendor/libs/typeahead-js/typeahead.css" />
+  <link rel="stylesheet" href="{{asset('/assets/vendor/libs/node-waves/node-waves.css')}}" />
+  <link rel="stylesheet" href="{{asset('/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
+  <link rel="stylesheet" href="{{asset('/assets/vendor/libs/typeahead-js/typeahead.css')}}" />
   <!-- Vendor -->
-  <link rel="stylesheet" href="../../assets/vendor/libs/@form-validation/umd/styles/index.min.css" />
+  <link rel="stylesheet" href="{{asset('/assets/vendor/libs/@form-validation/umd/styles/index.min.css')}}" />
 
   <!-- Page CSS -->
   <!-- Page -->
-  <link rel="stylesheet" href="../../assets/vendor/css/pages/page-auth.css">
+  <link rel="stylesheet" href="{{asset('/assets/vendor/css/pages/page-auth.css')}}">
 
   <!-- Helpers -->
-  <script src="../../assets/vendor/js/helpers.js"></script>
+  <script src="{{asset('/assets/vendor/js/helpers.js')}}"></script>
   <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
   <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
-  <script src="../../assets/vendor/js/template-customizer.js"></script>
+  <script src="{{asset('/assets/vendor/js/template-customizer.js')}}"></script>
   <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-  <script src="../../assets/js/config.js"></script>
+  <script src="{{asset('/assets/js/config.js')}}"></script>
 
 </head>
 
 <body>
+
 
 
   <!-- ?PROD Only: Google Tag Manager (noscript) (Default ThemeSelection: GTM-5DDHKGP, PixInvent: GTM-5J3LMKC) -->
@@ -83,19 +83,19 @@
   <!-- Content -->
 
   <div class="container-xxl">
-    <div class="authentication-wrapper authentication-basic container-p-y">
-      <div class="authentication-inner py-4">
+    <div class="authentication-wrapper authentication-basic container-p-y" style="background-color: #D1C0D3;">
+      <div class="authentication-inner py-10">
         <!-- Login -->
         <div class="card">
           <div class="card-body">
             <!-- Logo -->
-            <div class="app-brand justify-content-center mb-4 mt-2">
+            <div class="app-brand justify-content-center mb-3 mt-3" style="background-color: #FCFBF7;">
               <a href="index.html" class="app-brand-link gap-2">
-                  <img src="../../public/img/commercer.png" alt="Logo Perrón">
+                <img src="{{asset('img/icon.png')}}" alt=  >
               </a>
             </div>
             <!-- /Logo -->
-            <h4 class="mb-1 pt-2">Bienvenido a WebStore! 👋</h4>
+            <h4 class="mb-1 pt-2">Bienvenido a WebStore!📦</h4>
             <p class="mb-4"> Inicia la Aventura</p>
 
             <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
@@ -105,7 +105,7 @@
               </div>
               <div class="mb-3 form-password-toggle">
                 <div class="d-flex justify-content-between">
-                  <label class="form-label" for="password">Password</label>
+                  <label class="form-label" for="password">Contraseña</label>
                   <a href="auth-forgot-password-basic.html">
                     <small>¿Olvidó su contraseña?</small>
                   </a>
@@ -148,29 +148,29 @@
   <!-- Core JS -->
   <!-- build:js assets/vendor/js/core.js -->
 
-  <script src="../../assets/vendor/libs/jquery/jquery.js"></script>
-  <script src="../../assets/vendor/libs/popper/popper.js"></script>
-  <script src="../../assets/vendor/js/bootstrap.js"></script>
-  <script src="../../assets/vendor/libs/node-waves/node-waves.js"></script>
-  <script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-  <script src="../../assets/vendor/libs/hammer/hammer.js"></script>
-  <script src="../../assets/vendor/libs/i18n/i18n.js"></script>
-  <script src="../../assets/vendor/libs/typeahead-js/typeahead.js"></script>
-  <script src="../../assets/vendor/js/menu.js"></script>
+  <script src="{{asset('/assets/vendor/libs/jquery/jquery.js')}}"></script>
+  <script src="{{asset('/assets/vendor/libs/popper/popper.js')}}"></script>
+  <script src="{{asset('/assets/vendor/js/bootstrap.js')}}"></script>
+  <script src="{{asset('/assets/vendor/libs/node-waves/node-waves.js')}}"></script>
+  <script src="{{asset('/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+  <script src="{{asset('/assets/vendor/libs/hammer/hammer.js')}}"></script>
+  <script src="{{asset('/assets/vendor/libs/i18n/i18n.js')}}"></script>
+  <script src="{{asset('/assets/vendor/libs/typeahead-js/typeahead.js')}}"></script>
+  <script src="{{asset('/assets/vendor/js/menu.js')}}"></script>
 
   <!-- endbuild -->
 
   <!-- Vendors JS -->
-  <script src="../../assets/vendor/libs/@form-validation/umd/bundle/popular.min.js"></script>
-  <script src="../../assets/vendor/libs/@form-validation/umd/plugin-bootstrap5/index.min.js"></script>
-  <script src="../../assets/vendor/libs/@form-validation/umd/plugin-auto-focus/index.min.js"></script>
+  <script src="{{asset('/assets/vendor/libs/@form-validation/umd/bundle/popular.min.js')}}"></script>
+  <script src="{{asset('/assets/vendor/libs/@form-validation/umd/plugin-bootstrap5/index.min.js')}}"></script>
+  <script src="{{asset('/assets/vendor/libs/@form-validation/umd/plugin-auto-focus/index.min.js')}}"></script>
 
   <!-- Main JS -->
-  <script src="../../assets/js/main.js"></script>
+  <script src="{{asset('/assets/js/main.js')}}"></script>
 
 
   <!-- Page JS -->
-  <script src="../../assets/js/pages-auth.js"></script>
+  <script src="{{asset('/assets/js/pages-auth.js')}}"></script>
 
 </body>
 
