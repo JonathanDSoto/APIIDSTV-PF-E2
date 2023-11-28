c510c048135a258407249f8aa0f96c9619911e28
jgarciavega <jgarcia_20@alu.uabcs.mx>
2023-11-27 18:46:48 -0700

    Pagina ordenes agregada

--- proyectofinal/resources/views/ordenes.blade.php
+++ proyectofinal/resources/views/ordenes.blade.php
@@ -1,95 +1,149 @@
 @extends('layouts.layout')
 @section('content')
 
-<!DOCTYPE html>
-<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario de Orden</title>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-        }
+                    table {
+                width: 100%;
+                border-collapse: collapse;
+                margin-bottom: 20px;
+            }
 
-        #formulario {
-            display: flex;
-            width: 80%;
-        }
+            table,
+            th,
+            td {
+                border: 1px solid #ddd;
+            }
 
-        #producto-info, #cliente-info {
-            flex: 1;
-            padding: 20px;
-        }
+            th,
+            td {
+                padding: 10px;
+                text-align: center;
 
-        #producto-img {
-            width: 100%;
-            max-width: 200px;
-        }
+            }
 
-        #cantidad {
-            width: 50px;
-        }
+            th {
+                background-color: #f2f2f2;
+            }
+            .contenedor{
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
 
-        #enviar-btn {
-            width: 100%;
-        }
+
+            }
+            .btn-agregar{
+                background-color: cornflowerblue;
+                color:white;
+                padding: 5px 10px;
+            }
+            .btn-agregar:hover{
+                color: yellow;
+            }
+            .btn-borrar{
+                background-color: red;
+                color:white;
+                padding: 5px 10px;
+            }
+            .btn-editar{
+                background-color: blueviolet;
+                color:white;
+                padding: 5px 10px;
+            }
+
+
+
     </style>
 </head>
 <body>
 
-    <div id="formulario">
-        <div id="producto-info">
-            <h2>Producto</h2>
-            <img src="{{ asset('url_de_la_imagen_del_producto.jpg') }}" alt="Imagen del Producto" id="producto-img">
-            <p>Precio: $100.00</p>
-            <label>
-                <input type="radio" name="producto" value="producto1"> Producto 1
-            </label>
-            <label>
-                <input type="radio" name="producto" value="producto2"> Producto 2
-            </label>
-            <!-- Agrega más opciones de productos según sea necesario -->
-        </div>
 
-        <div id="producto-detalle">
-            <label for="cantidad">Cantidad:</label>
-            <input type="number" id="cantidad" name="cantidad" min="1" value="1" required>
-
-            <p>Descripción del Producto: Lorem ipsum dolor sit amet, consectetur adipiscing elit....</p>
-        </div>
-
-        <div id="cliente-info">
-            <h2>Datos del Cliente</h2>
-            <form action="#" method="post">
-                @csrf
-                <label for="nombre">Nombre:</label>
-                <input type="text" id="nombre" name="nombre" required>
-
-                <label for="apellidos">Apellidos:</label>
-                <input type="text" id="apellidos" name="apellidos" required>
-
-                <label for="telefono">Teléfono:</label>
-                <input type="tel" id="telefono" name="telefono" required>
-
-                <label for="email">Correo electrónico:</label>
-                <input type="email" id="email" name="email" required>
-
-                <label for="direccion">Dirección:</label>
-                <textarea id="direccion" name="direccion" required></textarea>
-
-                <button type="submit" id="enviar-btn">Enviar Orden</button>
-            </form>
-        </div>
+    <div class="contenedor">
+        <h2>Ordenes</h2>
+        <a class="btn-agregar" href="">Agregar Orden</a>
     </div>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Estado</th>
+                <th>Cliente</th>
+                <th>Fecha de creacion</th>
+                <th>Cupon</th>
+                <th>Subtotal</th>
+                <th>Total</th>
+                <th>Accion</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <tr>
+                <td>Completado</td>
+                <td>Irbin</td>
+                <td>27/11/2023</td>
+                <td>50%</td>
+                <td>$2000</td>
+                <td>$1000</td>
+                <td>
+                    <a class="btn-borrar" href="">Borrar</a> 
+                    <a class="btn-editar" href="">Detalles</a> 
+                </td>
+
+            </tr>
+            <tr>
+                <td>Completado</td>
+                <td>Irbin</td>
+                <td>27/11/2023</td>
+                <td>50%</td>
+                <td>$2000</td>
+                <td>$1000</td>
+                <td>
+                    <a class="btn-borrar" href="">Borrar</a> 
+                    <a class="btn-editar" href="">Detalles</a> 
+                </td>
+
+            </tr>
+            <tr>
+                <td>Completado</td>
+                <td>Irbin</td>
+                <td>27/11/2023</td>
+                <td>50%</td>
+                <td>$2000</td>
+                <td>$1000</td>
+                <td>
+                    <a class="btn-borrar" href="">Borrar</a> 
+                    <a class="btn-editar" href="">Detalles</a> 
+                </td>
+
+            </tr>
+            <tr>
+                <td>Completado</td>
+                <td>Irbin</td>
+                <td>27/11/2023</td>
+                <td>50%</td>
+                <td>$2000</td>
+                <td>$1000</td>
+                <td>
+                    <a class="btn-borrar" href="">Borrar</a> 
+                    <a class="btn-editar" href="">Detalles</a> 
+                </td>
+
+            </tr>
+
+
+        </tbody>
+    </table>
+
+
 
 </body>
 </html>
+
+
 
 
 
